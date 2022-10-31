1642ad7817aec6aad2b3abfbad2ad7a89c5eb776
niekschoone <niek.schoone@quicknet.nl>
2022-10-31 17:47:52 +0100

    extra describe in unit test

--- test/object-validation_spec.ts
+++ test/object-validation_spec.ts
@@ -6,61 +6,69 @@ import { personObj, personObjF, personSchema } from "../examples/persons";
 import { carObj, carObjF, carSchema } from "../examples/cars";
 
 describe("ValidationUtils", () => {
-  context(
-    "When an object is missing keys that are specified in the schema",
-    () => {
-      it("Should return false if personObjF is compared to personSchema", () => {
+  describe("ValidateObjectBySchema", () => {
+    context(
+      "When an object is missing keys that are specified in the schema",
+      () => {
+        it("Should return false if personObjF is compared to personSchema", () => {
+          const result = ValidationUtils.ValidateObjectBySchema(
+            personObjF,
+            personSchema
+          );
+          expect(result).to.equal(false);
+        });
+      }
+    );
+    context("When a property in an object is of the wrong type", () => {
+      it("Should return false if 'array' is passed where 'object' is expected", () => {
         const result = ValidationUtils.ValidateObjectBySchema(
-          personObjF,
+          barObjF,
+          barSchema
+        );
+        expect(result).to.equal(false);
+      });
+      it("Should return false if 'string' is passed where 'number' is expected", () => {
+        const result = ValidationUtils.ValidateObjectBySchema(
+          carObjF,
+          carSchema
+        );
+        expect(result).to.equal(false);
+      });
+    });
+    context("When the wrong schema is passed", () => {
+      it("Should return false if barObj is compared to personSchema", () => {
+        const result = ValidationUtils.ValidateObjectBySchema(
+          barObj,
           personSchema
         );
         expect(result).to.equal(false);
       });
-    }
-  );
-  context("When a property in an object is of the wrong type", () => {
-    it("Should return false if 'array' is passed where 'object' is expected", () => {
-      const result = ValidationUtils.ValidateObjectBySchema(barObjF, barSchema);
-      expect(result).to.equal(false);
     });
-    it("Should return false if 'string' is passed where 'number' is expected", () => {
-      const result = ValidationUtils.ValidateObjectBySchema(carObjF, carSchema);
-      expect(result).to.equal(false);
-    });
+    context(
+      "When all properties in an object are of the correct type specified in the schema",
+      () => {
+        it("Should return true with parameters personObj & personSchema", () => {
+          const result = ValidationUtils.ValidateObjectBySchema(
+            personObj,
+            personSchema
+          );
+          expect(result).to.equal(true);
+        });
+        it("Should return true with parameters barObj & barSchema", () => {
+          const result = ValidationUtils.ValidateObjectBySchema(
+            barObj,
+            barSchema
+          );
+          expect(result).to.equal(true);
+        });
+        it("Should return true with parameters carObj & carSchema", () => {
+          const result = ValidationUtils.ValidateObjectBySchema(
+            carObj,
+            carSchema
+          );
+          expect(result).to.equal(true);
+        });
+      }
+    );
   });
-  context("When the wrong schema is passed", () => {
-    it("Should return false if barObj is compared to personSchema", () => {
-      const result = ValidationUtils.ValidateObjectBySchema(
-        barObj,
-        personSchema
-      );
-      expect(result).to.equal(false);
-    });
-  });
-  context(
-    "When all properties in an object are of the correct type specified in the schema",
-    () => {
-      it("Should return true with parameters personObj & personSchema", () => {
-        const result = ValidationUtils.ValidateObjectBySchema(
-          personObj,
-          personSchema
-        );
-        expect(result).to.equal(true);
-      });
-      it("Should return true with parameters barObj & barSchema", () => {
-        const result = ValidationUtils.ValidateObjectBySchema(
-          barObj,
-          barSchema
-        );
-        expect(result).to.equal(true);
-      });
-      it("Should return true with parameters carObj & carSchema", () => {
-        const result = ValidationUtils.ValidateObjectBySchema(
-          carObj,
-          carSchema
-        );
-        expect(result).to.equal(true);
-      });
-    }
-  );
 });
